cecf025fd16e481228986f29e1303cb0fef03044
Ahmad Hasan <aaahmaaad.hsn@gmail.com>
2025-05-04 14:47:58 +0700

    refactor: update database connection to use environment variables

--- models/db.js
+++ models/db.js
@@ -1,11 +1,10 @@
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // ganti sesuai setting MySQL kamu
-  database: 'amerta_db',
-});
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,});
 
 db.connect((err) => {
   if (err) throw err;
